9c7449bb1410036cbdd084ed36c3509f4023a63c
jaspyplayz <jaspyplayz@gmail.com>
2025-02-14 14:45:37 -0500

    Solved Lesson4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,24 @@
+from math import ceil
 # Lesson 4
+
+#Input
+section1 = input("Enter section 1: ")
+section2 = input("Enter section 2: ")
+section3 = input("Enter section 3: ")
+
+#Processing 
+
+cans = len(section1) + len(section2) + len(section3)
+
+cansReq = ceil(cans / 12)
+
+leftOver = cansReq*12-cans
+
+price = cansReq * 14.95
+
+#Output
+
+print(cansReq)
+print(leftOver)
+print(price)
+
