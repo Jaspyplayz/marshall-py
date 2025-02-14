2298008470b01d9434e7ae28a755dfd0ffd4cfe5
jaspyplayz <jaspyplayz@gmail.com>
2025-02-14 14:20:50 -0500

    Solved Lesson3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,17 @@
+#import statements
+from math import sqrt, floor
+
 # Lesson 3
+tiles = int(input()) #int() is a type of casting Function that converts its argument to integer
+
+#processing 
+
+# answer = tiles ** 0.5
+# answer = floor(sqrt(tiles))
+answer = sqrt(tiles)
+answer = floor(answer)
+
+#output
+
+print(f"The largest square had a side length of {answer}.")
+
