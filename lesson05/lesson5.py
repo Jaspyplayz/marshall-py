39b9548850c0f32e45bd8460bff2f3d7a0b4a2bd
jaspyplayz <jaspyplayz@gmail.com>
2025-02-20 14:28:59 -0500

    Lesson5 solved

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,29 @@
 # Lesson 5
+
+# input 
+start_money = float(input())
+cookies_sold = input()
+
+#processing
+#big_cookies = 0 
+#cookies_sold = 0
+big_cookies = cookies_sold.count('b')
+cookies = cookies_sold.count('c')
+
+#for current_coookie in cookies_sold:
+#    if current_cookie == "c":
+#        cookies +=1
+#    elif current_cookie == "b":
+#        big_cookies += 1
+#    else:
+#        print(f"{current_cookie} is not a valid sale item.")
+# end of for
+
+total_cookies = big_cookies + cookies
+profit = (big_cookies * 1.25) + (cookies * 0.75)
+end_day = start_money + profit
+
+#output
+
+print(f"We sold {total_cookies} cookies, we made ${profit}. We now have ${end_day}.")
+
