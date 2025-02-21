ab2261285ba5e9ce7bf0a6e02ef7165f6ed99bf1
jaspyplayz <jaspyplayz@gmail.com>
2025-02-21 13:50:59 -0500

    Lesson10 solved

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,16 @@
 # Lesson 10
+
+teleNum = ''
+
+for i in range(4):
+    teleNum = teleNum + input()
+
+print(f"{teleNum}")
+
+if phone[0] in {'8', '9'}:
+
+    if phone[-1] in {'8', '9'}:
+        if phone[1] == phone[2]:
+            print(f"The phone number with {phone} as its last four digits is a telemarketer")
+        else:
+            print(f"The phone number with {phone} as its last four digits is not a telemarketer")
