bf5b8cd5ed9ce79d758cbf60867ee3f59563d8d4
jaspyplayz <jaspyplayz@gmail.com>
2025-02-21 14:17:42 -0500

    Lesson13 solved

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,11 @@
 # Lesson 13
+
+month = int(input())
+day = int(input())
+
+if month == 2 and day == 18:
+    print("Special Day")
+elif month == 1 or (month == 2 and day < 18):
+    print("Before")
+else:
+    print("After")
