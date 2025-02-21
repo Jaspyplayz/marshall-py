a40b07ff496baca85061d49cf0226c89cdc2fba9
jaspyplayz <jaspyplayz@gmail.com>
2025-02-21 09:30:42 -0500

    Lesson9 solved

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,33 @@
 # Lesson 9
+
+from random import choice
+
+invalid = True
+player = ''
+
+while invalid:
+    player = input("Enter a choice of (rock, paper, or scissors): ")
+    
+    if player in ('rock', 'paper', 'scissors'):
+        invalid = False
+
+cpu = choice(['rock', 'paper', 'scissors'])
+
+if player == cpu:
+    print("Tie Game")
+else:
+    if player == 'rock':
+        if cpu == 'paper':
+            print("CPU has won")
+        else:
+            print("The Player has won")
+    elif player == 'paper':
+        if cpu == 'scissors':
+            print("CPU has won")
+        else:
+            print("The player has won")
+    else:
+        if cpu == 'rock':
+            print("CPU has won")
+        else:
+            print("The player has won")
