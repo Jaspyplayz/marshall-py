47f99f26f14b2f8d9d4e6b51514fba147551166a
jaspyplayz <jaspyplayz@gmail.com>
2025-02-21 14:00:40 -0500

    Lesson11 solved

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,23 @@
 # Lesson 11
+
+point = input()
+
+point = point.split(' ')
+
+point = list(map(int, point))
+print(point)
+
+x, y = point
+print(f'x is {x}')
+print(f'y is {y}')
+
+if x > 0:
+    if y > 0:
+        print("Quadrant 1")
+    else:
+        print("Quadrant 4")
+else:
+    if y > 0:
+        print("Quadrant 2")
+    else: 
+        print("Quadrant 3")
