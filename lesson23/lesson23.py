57a945597f1b31cd5e6c102a77b2df2333de9879
jaspyplayz <jaspyplayz@gmail.com>
2025-02-24 14:29:13 -0500

    Lesson23 solved

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,25 @@
 # Lesson 23
+
+loop = True
+
+total_sum = 0
+counter = 0
+
+while loop:
+
+    user_input = input()
+
+    if user_input.lower().capitalize() == "Exit":
+        loop = False
+        break
+    else:
+        mark = int(user_input)
+        if 0 <= mark <= 100:
+            total_sum = total_sum + mark
+            counter += 1
+        else: 
+            print("Invalid input")
+
+average = total_sum/counter
+
+print(f"Your average is: {average}")
