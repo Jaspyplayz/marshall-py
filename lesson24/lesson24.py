44b34cf9bb5d1ef9cac07b21e9c80df0eb95c3bc
Jaspyplayz <jaspyplayz@gmail.com>
2025-02-24 19:18:51 -0500

    Lesson24 solved

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,20 @@
 # Lesson 24
+
+
+
+loop = True
+longestName = ""
+longestNum = 0
+
+while loop:
+    str = input()
+
+    if(str.lower().upper() == "X"):
+        loop = False
+        break
+    else:
+        if len(str) > len(longestName):
+            longestName = str
+            longestNum = len(str)
+        
+print(f"The longest name is {longestName} with {longestNum} letters")
