278d5cf3d5a35750abe2753d6f48d3c6502237e3
Jasper Li <jasper@SEA-FCMW5J3.EDU.YRDSB.NET>
2025-03-19 13:50:07 -0400

    Lesson28 solved

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,17 @@
 # Lesson 28
+
+def palindrome_checker(text) -> bool:
+
+    if len(text) == 1:
+        return True
+    else:
+        
+        mid = len(text) // 2
+
+        for i in range(mid):
+            if text[i] != text[-1*i-1]:
+                return False
+
+        return True
+
+print(palindrome_checker(input()))
