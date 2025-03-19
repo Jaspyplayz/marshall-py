bc65206d4f23b1d4df43bd651593f8705e00064f
Jasper Li <jasper@SEA-FCMW5J3.EDU.YRDSB.NET>
2025-03-19 13:53:32 -0400

    Lesson29 solved

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,16 @@
 # Lesson 29
+
+def consonant_counter(text):
+
+    counter = 0
+
+    text = text.lower()
+
+    for i in text:
+        if i.isalpha():
+            if i not in "aeiou":
+                counter += 1
+    
+    return counter
+
+print(consonant_counter(input()))
