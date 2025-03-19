34fe0210fa0d2e4f5de28777e0db59511ba974e5
Jasper Li <jasper@SEA-FCMW5J3.EDU.YRDSB.NET>
2025-03-19 13:49:36 -0400

    Lesson27 solved

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,19 @@
 # Lesson 27
+
+def string_cleaner(s):
+    
+    str = ""
+    
+    if len(s) == 0:
+        return -1
+    else:
+        for i in s:
+            if i.isalpha():
+                str += i.lower()
+        return str
+
+    
+value = string_cleaner(input())
+print(value)
+
+        
